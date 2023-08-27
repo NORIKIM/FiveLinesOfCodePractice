063e1dc13f1014fb706e08aa175eba57ea37b024
NORIKIM <jinagold.k@gmail.com>
2023-08-27 15:32:34 +0900

    긴 코드 조각내기-메서드 분리

--- index.ts
+++ index.ts
@@ -113,36 +113,48 @@ function update() {
 }
 
 function draw() {
+  let canvas = createCanvas();
+
+  drawMap(canvas);
+  drawPlayer(canvas);
+}
+
+function createCanvas() {
   let canvas = document.getElementById("GameCanvas") as HTMLCanvasElement;
   let g = canvas.getContext("2d");
 
   g.clearRect(0, 0, canvas.width, canvas.height);
-  
-  drawMap(g);
-  drawPlayer(g);
-  
+
+  return g;
 }
 
-function drawMap(g: CanvasRenderingContext2D) {
+function drawMap(canvas: CanvasRenderingContext2D) {
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
-      if (map[y][x] === Tile.FLUX)
-        g.fillStyle = "#ccffcc";
-      else if (map[y][x] === Tile.UNBREAKABLE)
-        g.fillStyle = "#999999";
-      else if (map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE)
-        g.fillStyle = "#0000cc";
-      else if (map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX)
-        g.fillStyle = "#8b4513";
-      else if (map[y][x] === Tile.KEY1 || map[y][x] === Tile.LOCK1)
-        g.fillStyle = "#ffcc00";
-      else if (map[y][x] === Tile.KEY2 || map[y][x] === Tile.LOCK2)
-        g.fillStyle = "#00ccff";
-
-      if (map[y][x] !== Tile.AIR && map[y][x] !== Tile.PLAYER)
-        g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+      fillTileColor(canvas, x, y);
+      createTile(canvas, x, y);
     }
   }
+}
+
+function fillTileColor(canvas: CanvasRenderingContext2D, x: number, y: number) {
+  if (map[y][x] === Tile.FLUX)
+    canvas.fillStyle = "#ccffcc";
+  else if (map[y][x] === Tile.UNBREAKABLE)
+    canvas.fillStyle = "#999999";
+  else if (map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE)
+    canvas.fillStyle = "#0000cc";
+  else if (map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX)
+    canvas.fillStyle = "#8b4513";
+  else if (map[y][x] === Tile.KEY1 || map[y][x] === Tile.LOCK1)
+    canvas.fillStyle = "#ffcc00";
+  else if (map[y][x] === Tile.KEY2 || map[y][x] === Tile.LOCK2)
+    canvas.fillStyle = "#00ccff";
+}
+
+function createTile(canvas: CanvasRenderingContext2D, x: number, y: number) {
+  if (map[y][x] !== Tile.AIR && map[y][x] !== Tile.PLAYER)
+  canvas.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
 }
 
 function drawPlayer(g: CanvasRenderingContext2D) {
